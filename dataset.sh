b9ac3ac1e12108494003bbc2897dc4126bf983c3
kaykoe <karolkoenigg@gmail.com>
2026-06-07 17:22:21 +0200

    chore: adapt to running locally

---
+++ dataset.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+curl -L -o emotion-detection-fer.zip https://www.kaggle.com/api/v1/datasets/download/ananthu017/emotion-detection-fer
+unzip emotion-detection-fer.zip -d ./emotion-detection-fer/
